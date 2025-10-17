434b9c762db4462f59dc94ae8266b052f7116f7f
Ivan Ilnickiy <bullet_sun@mail.ru>
2025-10-17 23:59:53 +0300

    scx

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -117,7 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
